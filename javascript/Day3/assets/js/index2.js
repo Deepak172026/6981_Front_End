11d9d06cf21e687a4b64fe02a97c7f72e359386e
Deepak <deepak172026@gmail.com>
2026-09-24 00:28:21 +0530

    updat

---
+++ javascript/Day3/assets/js/index2.js
@@ -0,0 +1,97 @@
+// string
+
+let city = "vellore"
+console.log(city);
+city += " city"
+console.log(city)
+
+let name = "Samuvel"
+console.log(name);
+name += " Sherbin"
+console.log(name)
+
+
+let degree = "Beachelore"
+console.log(degree);
+degree += " Engineering"
+console.log(degree)
+
+let subject = "social"
+console.log(subject);
+subject += " science"
+console.log(subject)
+
+let it = "web"
+console.log(it);
+it += " developer"
+console.log(it)
+
+let cityPincode = "vellore"
+console.log(cityPincode);
+cityPincode += " 632 006"
+console.log(cityPincode)
+
+
+let course = "javascript"
+console.log(course);
+course += " developer"
+console.log(course)
+
+
+let name2 = "mahesh"
+console.log(name2);
+name2 += " waran"
+console.log(name2)
+
+let house = "Home"
+console.log(house);
+house += " Address"
+console.log(house)
+
+let center = "soft"
+console.log(center);
+center += " logic"
+console.log(center)
+
+let number = "phone"
+console.log(number);
+number += " Number"
+console.log(number)
+
+let method = "payment"
+console.log(method);
+method += " method"
+console.log(method)
+
+let website = "gaming"
+console.log(website);
+website += " website"
+console.log(website)
+
+let wish = "dream"
+console.log(wish);
+wish += " builders"
+console.log(wish)
+
+
+let name3 = "deepak"
+console.log(name3);
+name3 += " kumar"
+console.log(name3)
+
+
+let name4 = "lokesh"
+console.log(name4);
+name4 += "  waran"
+console.log(name4)
+
+let collegeName = "Engineering"
+console.log(collegeName);
+collegeName += " college"
+console.log(collegeName)
+
+let villageName = "senur"
+console.log(villageName);
+villageName += " village"
+console.log(villageName)
+
