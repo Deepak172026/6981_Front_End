d83bfd1fe801014ec0d524f7c4ec6ba0d8cbe29d
Deepak <deepak172026@gmail.com>
2026-09-23 23:48:39 +0530

    upd

---
+++ javascript/Day3/assets/js/index.js
@@ -0,0 +1,106 @@
+// A customer buys:
+
+// Shirt Price = 1200
+
+// Quantity = 3
+
+// Delivery Charge = 100
+
+// Calculate the product total and final bill.
+
+// Use:
+
+// *, +
+
+let shirtPrice = 1200
+let productQuantity = 3
+let deliveryCharge = 100
+let productTotal = (shirtPrice * productQuantity)
+console.log("product Total=" + productTotal );
+let finalBill = (shirtPrice * productQuantity + deliveryCharge)
+console.log("Final Bill=" + finalBill)
+
+// Employee Salary = 30000
+
+// Bonus = 5000
+
+// Add the bonus to the salary using +=.
+
+// Then deduct:
+
+// Tax = 2000
+
+// using -=.
+
+// Find the final salary.
+
+let employeeSalary = 30000
+let employeeBonus = 5000
+employeeSalary += employeeBonus
+console.log(employeeSalary);
+let tax = 2000
+employeeSalary -= tax 
+console.log("Final Salary = "+ employeeSalary);
+
+// Starting Balance = 20000
+
+// Deposit = 5000
+
+// Withdrawal = 3000
+
+// Update the balance using:
+
+// +=
+
+// -=
+
+// Print the final balance.
+
+let startingBalance = 20000
+let Deposit = 5000
+let withDrawal = 3000
+startingBalance += Deposit -= withDrawal
+console.log("Final Balance = "+ startingBalance);
+
+// Marks:
+// Tamil = 80
+// English = 75
+// Maths = 90
+// Science = 85
+// Computer = 95
+// Calculate:
+// Total Marks
+// Average Marks
+// Use:
+// +
+// /
+
+let Tamil = 80
+let English = 75
+let Maths = 90
+let Science = 95
+let Computer = 95
+let totalMark
+totalMark = Tamil + English + Maths + Science + Computer
+console.log("Total Mark ="+ totalMark);
+let averageMark
+averageMark = totalMark/5
+console.log("Average Mark = "+ averageMark);
+
+// Start with:
+// number = 20
+// Perform these operations one by one:
+// 1. Add 10 using +=
+// 2. Multiply by 2 using *=
+// 3. Subtract 20 using -=
+// 4. Divide by 4 using /=
+// 5. Find remainder after dividing by 3 using %=
+// Print the final value.
+
+let number = 20
+number += 10
+number *= 2
+number -= 20
+number /= 4
+number %= 3
+console.log("Final Value = "+ number);
