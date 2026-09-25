83a3df624323c133a7ebb99fbc11d5c34c6d17ef
Deepak <deepak172026@gmail.com>
2026-09-25 08:54:08 +0530

    updated

--- javascript/Day4/assets/js/index.js
+++ javascript/Day4/assets/js/index.js
@@ -108,3 +108,101 @@ for(let a =1; a<=20; a++){
     }
 }
 
+// for (let a=2; a<=30; a++){
+//     console.log(a);
+// }
+// for (let b=2; b<=30; b++){
+//     console.log(b);
+// }
+// for (let c=3; c<=30; c++){
+//     console.log(c);
+// }
+// for (let d=4; d<=30; d++){
+//     console.log(d);
+// }
+// for (let e=5; e<=30; e++){
+//     console.log(e);
+// }
+// for (let f=6; f<=30; f++){
+//     console.log(f);
+// }
+// for (let g=7; g<=30; g++){
+//     console.log(g);
+    
+// }
+// for (let h=8; h<=30; h++){
+//     console.log(h);
+    
+// }
+// for(let i=9; i<=30; i++){
+//     console.log(i);
+    
+// }
+// for(let j=10; j<=30; j++){
+//     console.log(j);
+    
+// }
+// for(let k=11; k<=30; k++){
+//     console.log(k);
+    
+// }
+// for(let l=12; l<=30; l++){
+//     console.log(l);
+    
+// }
+// for(let m=13; m<=30; m++){
+//     console.log(m);
+    
+// }
+// for(let n=14; n<=14; n++){
+//     console.log(n);
+    
+// }
+// for(let o=15; o<=30; o++){
+//      console.log(o);
+     
+// }
+// for(let p=16; p<=30; p++){
+//     console.log(p);
+    
+// }
+// for(let q=17; q<=30; q++){
+//     console.log(q);
+// }
+
+// for (let r=18; r<=30; r++){
+//     console.log(r);
+    
+// }
+// for(let s=19; s<=30; s++){
+//     console.log(s);
+    
+// }
+// for(let t=20; t<=30; t++){
+//     console.log(t);
+    
+// }
+// for (let u=21; u<=30; u++){
+//     console.log(u);
+    
+// }
+// for (let v=22; v<=30; v++){
+//     console.log(v);
+    
+// }
+// for (let w=23; w<=30; w++){
+//     console.log(w);
+    
+// }
+// for (let x=24; x<=30; x++){
+//     console.log(x);
+    
+// }
+// for (let y=25; y<=30; y++){
+//     console.log(y);
+    
+// }
+// for (let z=26; z<=30; z++){
+//     console.log(z);
+    
+// }
